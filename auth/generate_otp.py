6ef14e814553baa8d8e9f1e581cdbb9a30638a3f
PRAVEEN-CS33 <praveen@qoruz.com>
2025-02-20 23:00:36 +0530

    Removed sensitive data

--- auth/generate_otp.py
+++ auth/generate_otp.py
@@ -2,6 +2,8 @@ import random
 import secrets
 import string
 from twilio.rest import Client
+import os
+
 
 def generate_secure_otp(number):
     characters = string.digits 
@@ -9,8 +11,8 @@ def generate_secure_otp(number):
 
     str="Your OTP for Verification is "+otp
 
-    account_sid = 'AC138692c8f4c7ae6c08a9a86e934c3734'
-    auth_token = '764c09c702781897d67811048075df9c'
+    TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+    HUGGING_FACE_TOKEN = os.getenv("HUGGING_FACE_TOKEN")
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         from_='+14405652156',
